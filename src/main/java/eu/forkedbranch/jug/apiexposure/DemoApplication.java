128a995575b4e2b0c20472be04ac2b39bf0f451e
bartek <bartek@DESKTOP-ETGCOHD>
2017-12-13 20:17:51 +0100

    PART 2: Swagger Bean Initialized

--- src/main/java/eu/forkedbranch/jug/apiexposure/DemoApplication.java
+++ src/main/java/eu/forkedbranch/jug/apiexposure/DemoApplication.java
@@ -3,5 +3,20 @@ package eu.forkedbranch.jug.apiexposure;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 @ApplicationPath("/api")
-public class DemoApplication extends Application { }
+public class DemoApplication extends Application {
+	private final BeanConfig swagger;
+
+	public DemoApplication() {
+		swagger = new BeanConfig();
+		swagger.setTitle("Rest Demo");
+		swagger.setVersion("v1");
+		swagger.setSchemes(new String[] { "http" });
+		swagger.setHost("localhost:8080");
+		swagger.setBasePath("/api");
+		swagger.setResourcePackage("eu.forkedbranch.jug.apiexposure");
+		swagger.setScan(true);
+	}
+}

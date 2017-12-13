7fbf37797b55504aac939ca70cb6819023dcc2de
bartek <bartek@DESKTOP-ETGCOHD>
2017-12-13 20:45:50 +0100

    PART 3: Extended Swagger Annotations

--- src/main/java/eu/forkedbranch/jug/apiexposure/controller/UsersController.java
+++ src/main/java/eu/forkedbranch/jug/apiexposure/controller/UsersController.java
@@ -18,13 +18,25 @@ import javax.ws.rs.core.UriInfo;
 
 import eu.forkedbranch.jug.apiexposure.models.User;
 import eu.forkedbranch.jug.apiexposure.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
+@Api
 @Path("/users")
 public class UsersController {
 	private UserService service = UserService.getInstance();
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(
+			value = "Get all users",
+			notes = "Some more elaborate text on getting all users",
+			response = User.class,
+			responseContainer = "List"
+			)
 	public Response getUsers()
 	{
 		return Response.ok().entity(service.getUsers()).build();
@@ -33,6 +45,17 @@ public class UsersController {
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(
+			value = "Get user by ID",
+			notes = "Some more elaborate text on getting a user by ID",
+			response = User.class
+			)
+	@ApiResponses(
+		@ApiResponse(
+				code = 404,
+				message = "User resource not found"
+				)
+		)
 	public Response getUser(
 			@PathParam("id") String id) 
 	{
@@ -46,8 +69,20 @@ public class UsersController {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@ApiOperation(
+			value = "Add user",
+			notes = "Some more elaborate text on adding a user"
+			)
+	@ApiResponses(
+			@ApiResponse(
+					code = 201, 
+					message = "User successfuly created"
+					)
+			)
 	public Response addUser(
+			@ApiParam(required = true, value="First name of a user")
 			@FormParam("firstName") String firstName,
+			@ApiParam(required = true, value="Last name of a user")
 			@FormParam("lastName") String lastName,
 			@Context UriInfo info)
 	{
@@ -63,6 +98,17 @@ public class UsersController {
 	@DELETE
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(
+			value = "Delete user by ID",
+			notes = "Some more elaborate text on deleting a user by ID",
+			response = User.class
+			)
+	@ApiResponses(
+		@ApiResponse(
+				code = 404,
+				message = "User resource not found"
+				)
+		)
 	public Response deleteUser(
 			@PathParam("id") String id) 
 	{
--- src/main/java/eu/forkedbranch/jug/apiexposure/models/User.java
+++ src/main/java/eu/forkedbranch/jug/apiexposure/models/User.java
@@ -1,5 +1,11 @@
 package eu.forkedbranch.jug.apiexposure.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(
+		description = "User resource model"
+		)
 public class User {
 	private String firstName;
 	private String lastName;
@@ -14,16 +20,28 @@ public class User {
 		this.id = id;
 	}
 
+	@ApiModelProperty(
+			value = "user first name",
+			required = true
+			)
 	public String getFirstName() {
 		return firstName;
 	}
 
+	@ApiModelProperty(
+			value = "user last name"
+			)
 	public String getLastName() {
 		return lastName;
 	}
 
+	@ApiModelProperty(
+			value = "user ID",
+			dataType = "string",
+			example = "905a9d3f-87e8-499e-9d56-25472abf4e1c"
+			)
 	public String getId() {
 		return id;
-	}
-		
+	}		
 }
+
